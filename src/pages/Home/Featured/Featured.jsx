59aac466e611a0512fb0967ecc64a74ad2c733f0
tofayeLL <ahmedtofayel132@gmail.com>
2024-05-16 13:52:32 +0600

    added paralax and set backround image linear gradient

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -4,26 +4,32 @@ import './Featured.css'
 
 const Featured = () => {
     return (
-        <section className="featured-item py-8">
-            <div>
-                <SectionTitle
-                subheading={"---Check it out---"}
-                heading={"FROM OUR MENU"}                                  
-                ></SectionTitle>
-            </div>
+        <section className="featured-item py-0 bg-fixed ">
+            <div className="bg-[linear-gradient(45deg,rgba(0,0,0,0.5),rgba(0,0,0,0.7))]">
 
-
-            <div className="flex justify-center items-center gap-8 py-10 px-40 text-white">
-                <div className="flex-1">
-                    <img src={featuredImg} alt="" className="" />
+                <div className="pt-8 text-white">
+                    <SectionTitle
+                        subheading={"---Check it out---"}
+                        heading={"FROM OUR MENU"}
+                    ></SectionTitle>
                 </div>
 
-                <div className="flex-1 space-y-3">
-                    <h4>March 20, 2024</h4>
-                    <h3>WHERE CAN I GET SOME?</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere, deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium tempore consequatur consequuntur omnis ullam maxime tenetur</p>
-                    <button className="p-3 border-b-2 btn btn-outline">Read More</button>
+
+                <div className="flex justify-center  items-center gap-8 py-10 px-40 text-white">
+                    <div className="flex-1">
+                        <img src={featuredImg} alt="" className="" />
+                    </div>
+
+                    <div className="flex-1 space-y-3">
+                        <h4>March 20, 2024</h4>
+                        <h3>WHERE CAN I GET SOME?</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere, deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium tempore consequatur consequuntur omnis ullam maxime tenetur</p>
+                        <button className="p-3 border-b-2 text-white btn btn-outline border-0">Read More</button>
+                    </div>
+
                 </div>
+
+
 
             </div>
 
