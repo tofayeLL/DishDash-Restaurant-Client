5f8be4658add1aeaf71d23111e33aa4e2fbdcfc4
tofayeLL <ahmedtofayel132@gmail.com>
2024-05-28 23:43:13 +0600

    finished to display bar charts and pie charts

--- src/pages/DashBoard/AdminHome/AdminHome.jsx
+++ src/pages/DashBoard/AdminHome/AdminHome.jsx
@@ -5,10 +5,15 @@ import revenue from '../../../assets/icon/revenue.png';
 import menuItems from '../../../assets/icon/customers.png';
 import products from '../../../assets/icon/products.png';
 import Orders from '../../../assets/icon/order.png';
-// for recharts
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Legend } from 'recharts';
+import { PieChart, Pie,  } from 'recharts';
 
+
+// for recharts
 const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+// for pie chart
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
 
 
 const AdminHome = () => {
@@ -54,6 +59,27 @@ const AdminHome = () => {
 
         return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
     };
+
+
+    // for pie charts
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+
+
+
+    const pieChartData = chartData.map(data => {
+        return { name: data.category , value: data.revenue}
+    })
 
 
 
@@ -131,7 +157,7 @@ const AdminHome = () => {
 
             {/* charts */}
 
-            <div className="flex items-center gap-8">
+            <div className="flex justify-center items-center">
                 {/* bar charts */}
                 <div>
                     <BarChart
@@ -154,6 +180,31 @@ const AdminHome = () => {
                             ))}
                         </Bar>
                     </BarChart>
+                </div>
+
+
+
+                {/* pie charts */}
+                <div>
+                   
+                        <PieChart width={500} height={500}>
+                            <Pie
+                                data={pieChartData}
+                                cx="50%"
+                                cy="50%"
+                                labelLine={false}
+                                label={renderCustomizedLabel}
+                                outerRadius={80}
+                                fill="#8884d8"
+                                dataKey="value"
+                            >
+                                {pieChartData.map((entry, index) => (
+                                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                ))}
+                            </Pie>
+                            <Legend></Legend>
+                        </PieChart>
+                 
                 </div>
 
 
